97081f26574ccb3b0f959a9a021c9fc5b28978e0
Yusuke Ikeda <yusuke_aono@yahoo.co.jp>
2021-03-08 15:20:25 +0900

    登録処理の実装;

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,36 +3,42 @@
 namespace App\Http\Controllers;
 
 use App\Article;
-use Illuminate\Http\ArticleRequest;
+use App\Http\Requests\ArticleRequest;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
 
-    public function index(){
+    public function index()
+    {
         $articles = Article::all()->sortByDesc('created_at');    
         return view('articles.index', ['articles'=>$articles]);
     }
 
-    public function create(){
+    public function create()
+    {
         return view('articles.create');
     }
 
-    public function store(ArticleRequest $request, Article $article){
-        $article->title   = $request->title;
-        $article->body    = $request->body;
+    public function store(ArticleRequest $request, Article $article)
+    {
+        $article->title = $request->title;
+        $article->body = $request->body;
         $article->user_id = $request->user()->id;
         $article->save();
         return redirect()->route('articles.index');
     }
 
-    public function show(){
+    public function show()
+    {
     }
 
-    public function edit(){
+    public function edit()
+    {
     }
 
-    public function update(){
+    public function update()
+    {
     }
 
     public function destroy(){
