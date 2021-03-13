8b65ba2e2cd8814d8257200df40826bda75c0062
Yusuke Ikeda <yusuke_aono@yahoo.co.jp>
2021-03-13 15:37:52 +0900

    いいねボタンの見た目実装

---
+++ resources/js/components/ArticleLike.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <button type="button" class="btn m-0 p-1 shadow-none">
+      <i class="fas fa-heart mr-1"/>
+    </button>
+    <span class="count">10</span>
+  </div>
+</template>
+
+<script>
+export default{
+
+}
+</script>
