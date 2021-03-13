1e8650a86611bdef1adcd6eaf742cbd54f1724d6
Yusuke Ikeda <yusuke_aono@yahoo.co.jp>
2021-03-13 16:51:53 +0900

    いいねボタンの実装(静的)

--- resources/js/components/ArticleLike.vue
+++ resources/js/components/ArticleLike.vue
@@ -1,14 +1,24 @@
 <template>
   <div>
     <button type="button" class="btn m-0 p-1 shadow-none">
-      <i class="fas fa-heart mr-1"/>
+      <i class="fas fa-heart mr-1" :class="{'red-text':this.isLikedBy}"/>
     </button>
     <span class="count">10</span>
   </div>
 </template>
 
 <script>
-export default{
-
-}
+export default {
+    props: {
+      initialIsLikedBy: {
+        type: Boolean,
+        default: false,
+      },
+    },
+    data() {
+      return {
+        isLikedBy: this.initialIsLikedBy,
+      }
+    },
+  }
 </script>
