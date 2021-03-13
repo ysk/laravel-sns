3f1d8ab2f8f7192c59bc8356d533761f20094536
Yusuke Ikeda <yusuke_aono@yahoo.co.jp>
2021-03-13 13:38:01 +0900

    パスワードのリセット実装

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,6 +26,11 @@
                   <label for="password">パスワード</label>
                   <input class="form-control" type="password" id="password" name="password" required>
                 </div>
+
+                <div class="text-left">
+                  <a href="{{ route('password.request') }}" class="card-text">パスワードを忘れた方</a>
+                </div>
+
                 <input type="hidden" name="remember" id="remember" value="on">
                 <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">ログイン</button>
               </form>
