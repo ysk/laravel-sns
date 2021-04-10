cfb524f749258d0f648151f251b25fec670078f6
Yusuke Ikeda <yusuke_aono@yahoo.co.jp>
2021-04-10 14:30:25 +0900

    いいねボタンアニメーション

--- resources/js/components/ArticleLike.vue
+++ resources/js/components/ArticleLike.vue
@@ -2,7 +2,7 @@
   <div>
     <button type="button" class="btn m-0 p-1 shadow-none">
       <i class="fas fa-heart mr-1" 
-      :class="{'red-text':this.isLikedBy}" 
+      :class="{'red-text':this.isLikedBy, 'animated heartBeat fast':this.gotToLike}"
       @click="clickLike"
       />
     </button>
@@ -33,6 +33,7 @@ export default {
       return {
         isLikedBy: this.initialIsLikedBy,
         countLikes: this.initialCountLikes,
+        gotToLike: false,
       }
     },
     methods: {
@@ -52,6 +53,7 @@ export default {
         
         this.isLikedBy = true;
         this.countLikes = response.data.countLikes;
+        this.gotToLike = true;
       },
 
       async unlike() {
@@ -59,6 +61,7 @@ export default {
 
         this.isLikedBy = false;
         this.countLikes = response.data.countLikes;
+        this.gotToLike = false;
       }
 
     }
