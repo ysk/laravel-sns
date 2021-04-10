a1cbf0b2184d563028ebf6b292119524b95afd8f
Yusuke Ikeda <yusuke_aono@yahoo.co.jp>
2021-04-10 12:31:59 +0900

    deploy test

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand navbar-dark blue-gradient">
 
-  <a class="navbar-brand" href="/"><i class="far fa-sticky-note mr-1"></i>memo</a>
+  <a class="navbar-brand" href="/"><i class="far fa-sticky-note mr-1"></i>memo2</a>
 
   <ul class="navbar-nav ml-auto">
 
