e529abf40968cc43a97953438d755a1b79fa378c
Yusuke Ikeda <yusuke_aono@yahoo.co.jp>
2021-04-23 00:08:49 +0900

    N+1問題対応

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -19,7 +19,7 @@ class ArticleController extends Controller
     //index
     public function index()
     {
-        $articles = Article::all()->sortByDesc('created_at');    
+        $articles = Article::all()->sortByDesc('created_at')->load(['user', 'likes', 'tags']); 
         return view('articles.index', ['articles'=>$articles]);
     }
 
