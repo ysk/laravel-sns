7e3ba736ecfdd07921117918b500b34db36f05e7
Yusuke Ikeda <yusuke_aono@yahoo.co.jp>
2021-04-22 23:58:01 +0900

    フォロー・フォロワーの一覧を表示

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,6 +18,39 @@ class UserController extends Controller
         ]);
     }
 
+    public function likes(string $name)
+    {
+        $user = User::where('name', $name)->first();
+        $articles = $user->likes->sortByDesc('created_at');
+
+        return view('users.likes', [
+            'user'     => $user,
+            'articles' => $articles,
+        ]);
+    }
+
+    public function followings(string $name)
+    {
+        $user = User::where('name', $name)->first();
+        $followings = $user->followings->sortByDesc('created_at');
+
+        return view('users.followings',[
+            'user' => $user,
+            'followings' => $followings,
+        ]);
+    }
+
+    public function followers(string $name)
+    {
+        $user = User::where('name', $name)->first();
+        $followers = $user->followers->sortByDesc('created_at');
+
+        return view('users.followers',[
+            'user' => $user,
+            'followers' => $followers,
+        ]);
+    }
+
     public function follow(Request $request, string $name)
     {
         $user = User::where('name', $name)->first();
@@ -35,6 +68,7 @@ class UserController extends Controller
     public function unfollow(Request $request, string $name)
     {
         $user = User::where('name', $name)->first();
+
         if($user->id === $request->user()->id)
         {
             return abort('404', 'Cannot follow yourself.');
--- resources/views/users/likes.blade.php
+++ resources/views/users/likes.blade.php
@@ -1,14 +1,14 @@
 @extends('app')
 
-@section('title', $user->name . 'が投稿した記事')
+@section('title', $user->name . 'のいいねした記事')
 
 @section('content')
   @include('nav')
   <div class="container">
     @include('users.user')
     @include('users.tabs', ['hasArticles' => true, 'hasLikes' => false])
-      @foreach($articles as $article)
-        @include('articles.card')
-      @endforeach
-    </div>
+    @foreach($articles as $article)
+      @include('articles.card')
+    @endforeach
+  </div>
 @endsection
