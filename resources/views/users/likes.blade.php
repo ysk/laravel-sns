35e5d0e523484057acef156fea5bbbe6e4cc5659
Yusuke Ikeda <yusuke_aono@yahoo.co.jp>
2021-04-26 17:53:54 +0900

    タブの切替修正

--- resources/views/users/likes.blade.php
+++ resources/views/users/likes.blade.php
@@ -6,7 +6,7 @@
   @include('nav')
   <div class="container">
     @include('users.user')
-    @include('users.tabs', ['hasArticles' => true, 'hasLikes' => false])
+    @include('users.tabs', ['hasArticles' => false, 'hasLikes' => true])
     @foreach($articles as $article)
       @include('articles.card')
     @endforeach
