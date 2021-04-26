5ca0ef0b74bf8f2b8f07f12f6fe4a73c0f13d1a3
Yusuke Ikeda <yusuke_aono@yahoo.co.jp>
2021-04-26 22:59:14 +0900

    テストコード実装

--- tests/Feature/ArticleTest.php
+++ tests/Feature/ArticleTest.php
@@ -18,12 +18,15 @@ class ArticleTest extends TestCase
 
     use RefreshDatabase;
 
+
     public function testIsLikedByNull()
     {
         $article = factory(Article::class)->create();
         $result = $article->isLikedBy(null);
+
         $this->assertFalse($result);
     }
+
 
     public function testIsLikedByTheUser()
     {
@@ -31,6 +34,20 @@ class ArticleTest extends TestCase
         $user = factory(User::class)->create();
         $article->likes()->attach($user);
         $result = $article->isLikedBy($user);
+
         $this->assertTrue($result);
     }
+
+
+    public function testIsLikedByAnother()
+    {
+        $article = factory(Article::class)->create();
+        $user = factory(User::class)->create();
+        $another = factory(User::class)->create();
+        $article->likes()->attach($another);
+        $result = $article->isLikedBy($user);
+
+        $this->assertFalse($result);
+    }
+
 }
