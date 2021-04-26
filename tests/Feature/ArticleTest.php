6068318d29138269e8ec85cd12c267f261040b14
Yusuke Ikeda <yusuke_aono@yahoo.co.jp>
2021-04-26 22:49:09 +0900

    テストコード実装

--- tests/Feature/ArticleTest.php
+++ tests/Feature/ArticleTest.php
@@ -2,10 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Article;
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-use App\Article;
 
 class ArticleTest extends TestCase
 {
@@ -22,5 +23,14 @@ class ArticleTest extends TestCase
         $article = factory(Article::class)->create();
         $result = $article->isLikedBy(null);
         $this->assertFalse($result);
+    }
+
+    public function testIsLikedByTheUser()
+    {
+        $article = factory(Article::class)->create();
+        $user = factory(User::class)->create();
+        $article->likes()->attach($user);
+        $result = $article->isLikedBy($user);
+        $this->assertTrue($result);
     }
 }
